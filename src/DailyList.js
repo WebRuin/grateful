50eb88e4033b2aca1fe1beadc2f4113493e15172
Tim Smith <tim.smith.hdg@gmail.com>
2018-07-15 10:25:27 -0700

    first commit

---
+++ src/DailyList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './DailyList.css';
+
+class DailyList extends Component {
+  render() {
+    return (
+      <div>Hi</div>
+    );
+  }
+}
+
+export default DailyList;
